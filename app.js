25a4ff42697cf39cad7a9f0005deacf953631052
El-Burnie <173103025+El-Burnie@users.noreply.github.com>
2024-07-05 00:41:44 -0400

    Add logic to fill in cells on mouseover

--- app.js
+++ app.js
@@ -6,14 +6,19 @@ const vhValue = 90;
 const container = document.querySelector("#container");
 
 function generateCells() {
-    const cellSize =  calculateCellSize()
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.style.width = `${cellSize}vh`;
-    cell.style.height = `${cellSize}vh`;
-    container.appendChild(cell);
+    const cellSize = calculateCellSize()
+    for (let i = 0; i < (gridSize ** 2); i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.style.width = `${cellSize}vh`;
+        cell.style.height = `${cellSize}vh`;
+        cell.addEventListener("mouseover", () => { 
+            cell.style.backgroundColor = "black";
+        });
+        container.appendChild(cell);
+    }
 }
 
 function calculateCellSize() {
-    return Math.round((vhValue / gridSize)*1000) / 1000;
+    return Math.round((vhValue / gridSize) * 10000) / 10000;
 }
